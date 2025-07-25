bec6e5b00689c92ed3ba315f5c17108b932b2ea6
Jonah Skinner <jonah.lee.skinner@gmail.com>
2025-07-25 17:27:22 +1000

    F1: Move the file structure and split up the api calls

--- F1Dash/composeApp/src/commonMain/kotlin/com/jskinner/f1dash/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.jskinner.f1dash
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- F1Dash/composeApp/src/commonMain/kotlin/com/jskinner/f1dash/di/modules/NetworkModule.kt
+++
@@ -1,7 +0,0 @@
-package com.jskinner.f1dash.di.modules
-
-import org.koin.core.module.Module
-
-expect val platformNetworkModule: Module
-
-val networkModule = platformNetworkModule
